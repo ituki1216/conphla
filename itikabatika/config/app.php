09fa871bda9622fb7ee5835fe9e6d248a147f401
ituki yamanaka <yamanakasan@icloud.com>
2024-12-07 23:59:59 +0900

    コメント

--- itikabatika/config/app.php
+++ itikabatika/config/app.php
@@ -80,7 +80,7 @@ return [
 
     'locale' => env('APP_LOCALE', 'en'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'ja'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'jaa'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'ja_JP'),
 
