a19b37968364801e7ae904e68a6f7709a83aa509
ituki yamanaka <yamanakasan@icloud.com>
2024-12-09 15:53:09 +0900

    laravelのルーティング定義

--- itikabatika/routes/web.php
+++ itikabatika/routes/web.php
@@ -1,4 +1,4 @@
-<?php
+<?php //web.phpはルーティングを行うfileでユーザーのHTTPリクエスト(例えばhome)などに対して、どのコントローラーやviewを表示するかを定義するためのもの
 
 use Illuminate\Support\Facades\Route;
 use App\\Http\\Controllers\\FormController;
@@ -16,5 +16,11 @@ use App\\Http\\Controllers\\FormController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+//お問い合わせfoam
+Route::get('contact', [FormController::class, "index"])->name['contact'];
+Route::post('contact.confirm', [FormController::class 'sendMail']);
+Route::get('contact.confirm', [FormController::class 'confirm'])->name('contact.confirm');
+Route::get('contact.complete', [FormController::class 'complete'])->name('contact.complete');
