9272b17f6e20024c35edae0600b995ef1b1213aa
ituki yamanaka <yamanakasan@icloud.com>
2024-12-12 16:32:22 +0900

    管理者養のメールを作成

--- itikabatika/app/Mail/ContactFormAdmin.php
+++ itikabatika/app/Mail/ContactFormAdmin.php
@@ -1,1 +1,36 @@
-https://depart-inc.com/blog/php-laravel-mailform/
+<?php
+
+namespace App\\Mail;
+
+use Illuminate\\Bus\\Queueable;
+use Illuminate\\Contracts\\Queue\\ShouldQueue;
+use Illuminate\\Mail\\Mailable;
+use Illuminate\\Mail\\Mailables\\Content;
+use Illuminate\\Mail\\Mailables\\Envelope;
+use Illuminate\\Mail\\Mailables\\Address;
+use Illuminate\\Queue\\SerializesModels;
+
+class ContactFormAdminMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(public array $form_data) //constructはコンストラクタメソッドでクラスのインスタンスが作成されたときに自動的に呼び出されます、
+    {
+        //
+    }
+
+    public function envelope(): envelope
+    {
+        $from = new Address($this->form['email'], $this->form_data['name']);
+        $subject = '[' . env('APP_NAME') . '] お問い合わせがありました';
+
+        return new Envelope(
+            from: $from,
+            subject: $subject,
+        );
+    }
+
+    public function content(
+        text: 'emails. contact.admin',
+    );
+}
